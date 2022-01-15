736ff94281256975dc13438a1eaf499b5077fdc8
Francisco Aiolfi <chicoaiolfi12@gmail.com>
2022-01-15 06:08:18 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GitHubIcon } from 'assets/img/github.svg'
+import './styles.css'
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/franciscoaiolfi">
+            <div className="dsmovie-contact-container">
+              <GitHubIcon />
+              <p className="dsmoive-contact-link">/DevSuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
